a0783f2c66e7404ace3d6cb2582ad3fa4e8e64b6
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-30 14:53:45 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(tripLength){
+  if (tripLength <= 400)
+  return 'This one is on me!'
+  else if (tripLength > 400 && tripLength <= 2000)
+  return 'That will be twenty bucks.'
+  else if (tripLength > 2000 && tripLength <= 2500)
+  return 'I will gladly take your thirty bucks.'
+  else
+  return 'No can do.'
+
+ }
+
+
+ const NYC = "NYC";
+
+ function ternaryCheckCity(City){
+ if (City === NYC){
+ return 'Ok, sounds good.'
+ } else {
+ return 'No go.'
+ }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip) {
+  if (Tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (Tip === 'not as generous') {
+    return 'Thank you.';
+  } else { 
+    return 'Bye.'
+  }
 }
